6ee972d4a0a7c0cbff455aded872ee4cc549e920
BhavyamDhandIsHere <144496307+BhavyamDhandIsHere@users.noreply.github.com>
2024-09-23 14:52:53 +0530

    creation of problem 6

---
+++ Problem 6/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <bitset>
+using namespace std;
+void LogFile(const string& event) {
+    ofstream BinFile("Machine_Code_Stack.txt",ios_base::app);
+    if (BinFile.is_open())
+    {
+        for (char c:event)
+        {
+            BinFile<<bitset<8>(c)<<" ";
+        }
+        BinFile<<endl;
+        BinFile.close();
+    }
+    
+}
